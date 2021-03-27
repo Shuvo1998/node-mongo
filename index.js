7746c201b67b324706c5620dd9b4db188d481b7f
Shuvo Kumer Ray <shuvokumerray@gmail.com>
2021-03-27 14:00:03 +0600

    middlewar cors added

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 8080;
+const users = ['Shuvo', 'Promoth', 'Paritosh', 'Rahul', 'Dipu'];
+app.use(cors());
 app.get('/', (req, res) => {
-    res.send('Root is calling')
+    const developer = {
+        name:'shuvo',
+        age : 22,
+        sex: 'male'
+    }
+    res.send(developer);
 })
-
+app.get('/job',(req, res) => {
+    res.send({job: 'software developer', salary:500000, location: 'Dhaka'})
+})
+app.get('/job/gf',(req, res) => {
+    res.send({gf: 'Tumpa Rani', salary:500000, location: 'Dhaka'})
+})
+app.get('/users/:id', (req, res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const name = users[id];
+    console.log(name)
+    res.send({id, name})
+})
 app.listen(port,() => console.log('Listening to port ' + port ))
